fc21fb9afaf3405e8ef043ec1ab6a310be471a6f
Victo Principe <victopessoa46@gmail.com>
2022-05-28 14:15:53 -0300

    Init app

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Application } from "express";
+import { Server } from "http";
+import "dotenv/config";
+
+class App {
+  private app!: Application;
+  private port!: Number;
+  private server!: Server;
+
+  public initApp(): void {
+    this.port = Number(process.env.PORT);
+    this.app = express();
+
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+
+    this.server = this.app.listen(this.port, (): void => {
+      console.log(`The app is listening on port ${this.port}`);
+    });
+  }
+
+  public shutdownApp() {
+    this.server.close();
+  }
+}
+
+export { App };
